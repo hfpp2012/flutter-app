c83d4f5409ca49179ebab335c398a21b5742f928
hfpp2012 <hfpp2012@gmail.com>
2020-10-27 12:23:51 +0800

    lifecycle

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,8 +6,17 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build function ran');
     return Scaffold(
         backgroundColor: Colors.grey[200],
         appBar: AppBar(
@@ -15,6 +24,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
           title: Text('choose a Location'),
           centerTitle: true,
         ),
-        body: Text('choose location'));
+        body:
+            RaisedButton(onPressed: () {}, child: Text('counter is $counter')));
   }
 }
